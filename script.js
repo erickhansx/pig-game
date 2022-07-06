c87dc0262355b046c74b700c4be14220699509ed
erickhansx <50700379+erickhansx@users.noreply.github.com>
2022-07-06 16:37:00 -0500

    Add variables for scores and btns, functionality to switch active player and style of the active player css

--- script.js
+++ script.js
@@ -3,8 +3,22 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//Rolling Dice Functionality
+
+btnRoll.addEventListener('click', () => {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled #1: if true, switch to next player.
+});
